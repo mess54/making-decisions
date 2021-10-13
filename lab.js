efd483fdd1db08f7bed6d38ee77f5392e371fc70
Jessica Menna <jessicaalexmenna@gmail.com>
2021-10-13 14:41:41 -0600

    finished problem 4

--- lab.js
+++ lab.js
@@ -25,7 +25,13 @@ var brittanisBirthYear = 1986
   Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. Handle this situation.
 */
 
-//CODE HERE
+if(amysAge < brittanisAge){
+  console.log("Brittani is older")
+} else if(amysAge === brittanisAge){
+  console.log("They are twins")
+} else {
+  console.log("Amy is older")
+} 
 
 
 ////////// PROBLEM 3 //////////
@@ -34,20 +40,32 @@ var brittanisBirthYear = 1986
   Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
 */
 
-//CODE HERE
+if(amysBirthYear === brittanisBirthYear){
+  console.log("Amy and Brittani were born in the same year")
+} else {
+  console.log("Amy and Brittani were not born in the same year")
+}
 
 
 ////////// PROBLEM 4 //////////
 
-let temperature = 55
-let rain = true
+let temperature = 85
+let rain = false
 
 /*
   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
 */
 
-//CODE HERE
+if(temperature >= 80 && rain === true){
+  console.log("Wear a t shirt and take an umbrella")
+} else if(temperature > 60 && temperature < 80 && rain === true){
+  console.log("Wear a rain jacket")
+} else if(temperature <= 60 && rain === true){
+  console.log("Wear a jacket and take an umbrella")
+} else if(rain === false && temperature >= 80){
+  console.log("Summertime clothing")
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -56,7 +74,6 @@ let rain = true
   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
 */
 
-//CODE HERE
 
 
 ////////// PROBLEM 6 //////////
