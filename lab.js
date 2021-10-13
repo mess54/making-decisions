61cd6ef6dd0389aa92d335d27692b1474f104e20
Jessica Menna <jessicaalexmenna@gmail.com>
2021-10-13 15:00:06 -0600

    Finished problem 8

--- lab.js
+++ lab.js
@@ -95,7 +95,9 @@ for(let i = 1; i <= 10; i++){
   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
 */
 
-//CODE HERE
+for(let i = 10; i >= 0; i--){
+  console.log(i)
+}
 
 let score = 0
 let passingScore = 7
@@ -106,7 +108,10 @@ let passingScore = 7
   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
 */
 
-//CODE HERE
+while(score < passingScore){
+  score++
+  console.log("Your score is not high enough")
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
